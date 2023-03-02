efcbf8a93fd825a3746cb57e8bea04fc876f54bb
hiluan <apt.muenchen@gmail.com>
2023-03-02 15:56:44 -0800

    Redux API for Essays

---
+++ src/config/explanation.js
@@ -0,0 +1,11 @@
+// Engine: text-davinci-003 is a good choice for generating high-quality and creative text, but it can be quite expensive. You may want to consider other models depending on your budget and needs.
+
+// Max tokens: This determines the maximum length of the generated text and should be set based on the desired length of the essay. You can experiment with different values to find the optimal length.
+
+// Temperature: This parameter controls the creativity and randomness of the generated text. A higher value will result in more creative and diverse responses, while a lower value will produce more predictable and conservative responses. Again, you can experiment with different values to find the optimal level of creativity for your use case.
+
+// Top p: This parameter determines the diversity of the generated text. A higher value will result in more diverse and unexpected responses, while a lower value will produce more predictable and similar responses. As with temperature, you can experiment with different values to find the optimal level of diversity for your use case.
+
+// Frequency penalty and presence penalty: These parameters control the repetition and relevance of the generated text, respectively. A higher value for frequency penalty will produce text with less repetition, while a higher value for presence penalty will produce text that is more closely related to the prompt. These parameters can be adjusted to fine-tune the quality of the generated text.
+
+// Overall, the optimal settings for generating well-written essays will depend on the specific requirements and goals of your project. You can experiment with different combinations of settings and perform tests to find the optimal configuration.
---
+++ src/state/api.js
@@ -0,0 +1,76 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const api = createApi({
+  reducerPath: "adminApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: process.env.REACT_APP_BASE_URL,
+  }),
+  tagTypes: [
+    "newEssay",
+    "getEssays",
+    "deleteEssays",
+    "getEssay",
+    "deleteEssay",
+    "newEssayChat",
+    "getEssayChats",
+  ],
+  endpoints: (build) => ({
+    // essays
+    newEssay: build.mutation({
+      query: (body) => ({
+        url: `/gpt/essays`,
+        method: "POST",
+        body,
+      }),
+      invalidatesTags: ["getEssays"],
+      providesTags: ["newEssay"],
+    }),
+    getEssays: build.query({
+      query: () => `/gpt/essays`,
+      providesTags: ["getEssays"],
+    }),
+    deleteEssays: build.mutation({
+      query: () => ({
+        url: `/gpt/essays`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["getEssays", "getEssay", "getEssayChats"],
+      providesTags: ["deleteEssays"],
+    }),
+    getEssay: build.query({
+      query: (essayId) => `/gpt/essays/${essayId}`,
+      providesTags: ["getEssay"],
+    }),
+    deleteEssay: build.mutation({
+      query: (essayId) => ({
+        url: `/gpt/essays/${essayId}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["getEssays", "getEssay", "getEssayChats"],
+      providesTags: ["deleteEssay"],
+    }),
+    newEssayChat: build.mutation({
+      query: (body) => ({
+        url: `/gpt/essays/${body.essayId}/chats`,
+        method: "POST",
+        body,
+      }),
+      invalidatesTags: ["getEssay", "getEssayChats"],
+      providesTags: ["newEssayChat"],
+    }),
+    getEssayChats: build.query({
+      query: (essayId) => `/gpt/essays/${essayId}/chats`,
+      providesTags: ["getEssayChats"],
+    }),
+  }),
+});
+
+export const {
+  newEssay,
+  getEssays,
+  deleteEssays,
+  getEssay,
+  deleteEssay,
+  newEssayChat,
+  getEssayChats,
+} = api;
